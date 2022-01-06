40f2e5de40bcea6bf89f2f31cef6bdad64e2a81d
Archer <archerevil2005@gmail.com>
2022-01-06 00:36:03 +0000

    Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'Tio seidosão'}
+    },
+    methods: {
+        GetCountry() {
+            
+        }
+    }
+});
+
+app.mount('#app');
